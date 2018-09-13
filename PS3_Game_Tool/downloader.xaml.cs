8f23bf97f7b1f97a3af73f7777be5904d0c2aa58
pink1 <pink1stools@users.noreply.github.com>
2018-09-12 22:28:32 -0500

    Add files via upload

---
+++ PS3_Game_Tool/downloader.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+
+namespace PS3_Game_Tool
+{
+    
+    public partial class downloader : MetroWindow
+    {
+
+        WebClient webClient;               // Our WebClient that will be doing the downloading for us
+        Stopwatch sw = new Stopwatch();    // The stopwatch which we will be using to calculate the download speed
+
+        static readonly string[] SizeSuffixes = { "bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+        static string SizeSuffix(Int64 value, int decimalPlaces = 1)
+        {
+            if (value < 0) { return "-" + SizeSuffix(-value); }
+
+            int c = 0;
+            decimal dValue = (decimal)value;
+            while (Math.Round(dValue, decimalPlaces) >= 1000)
+            {
+                dValue /= 1024;
+                c++;
+            }
+
+            return string.Format("{0:n" + decimalPlaces + "} {1}", dValue, SizeSuffixes[c]);
+        }
+
+
+        public downloader(string ID, string name)
+        {
+            InitializeComponent();
+            DownloadFile(ID, name);
+        }
+
+
+        private void startDownload(String ID, string name)
+        {
+            Thread thread = new Thread(() => {
+                WebClient client = new WebClient();
+                //client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+                //client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+                client.DownloadFileAsync(new Uri(ID), "updates/" + name);
+            });
+            thread.Start();
+        }
+
+        public void DownloadFile(string urlAddress, string location)
+        {
+            using (webClient = new WebClient())
+            {
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                // The variable that will be holding the url address (making sure it starts with http://)
+                Uri URL = urlAddress.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri("http://" + urlAddress);
+
+                // Start the stopwatch which we will be using to calculate the download speed
+                sw.Start();
+
+                try
+                {
+                    // Start downloading the file
+                    webClient.DownloadFileAsync(URL, location);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Calculate download speed and output it to labelSpeed.
+            labelSpeed.Content = string.Format("{0} kb/s", (e.BytesReceived / 1024d / sw.Elapsed.TotalSeconds).ToString("0.00"));
+
+            // Update the progressbar percentage only when the value is not the same.
+            progressBar.Value = e.ProgressPercentage;
+
+            // Show the percentage on our label.
+            labelPerc.Content = e.ProgressPercentage.ToString() + "%";
+
+            // Update the label with how much data have been downloaded so far and the total size of the file we are currently downloading
+            labelDownloaded.Content = string.Format("{0}'s / {1}'s",
+                SizeSuffix(e.BytesReceived),
+                SizeSuffix(e.TotalBytesToReceive));
+                //(e.BytesReceived / 1024d / 1024d).ToString("0.00"),
+                //(e.TotalBytesToReceive / 1024d / 1024d).ToString("0.00"));
+        }
+
+        // The event that will trigger when the WebClient is completed
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            // Reset the stopwatch.
+            sw.Reset();
+            this.Close();
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Download has been canceled.");
+            }
+            else
+            {
+                MessageBox.Show("Download completed!");
+            }
+
+            
+        }
+
+
+    }
+}
